bf3993bba97aaaeb92616d8af72f7fc2499f86eb
celina-ijs <celina.dev@instajs.com>
2023-03-24 18:04:47 +0700

    Init commit

---
+++ src/scconfig.json.ts
@@ -0,0 +1,10 @@
+export default {
+  "name": "@scom-video/main",
+  "version": "0.1.0",
+  "env": "",
+  "moduleDir": "src",
+  "main": "@scom-video/main",
+  "modules": {
+  },
+  "ipfsGatewayUrl": "https://ipfs.scom.dev/ipfs/"
+}
